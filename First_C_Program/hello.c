a6d32ed0006c5563f22a15acf44e48d3eeabd13b
jonahbardos <jonahbardos@gmail.com>
2019-03-27 21:05:34 -0700

    Added comments

--- First_C_Program/hello.c
+++ First_C_Program/hello.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include <stdio.h> // Tells the compiler to include information about the standing input and output library.
 
-int main(){
+// This function must be in every C Program because it is a COMPILER!
+// the "main" function is where all the codes gets executed.
+int main(){ 
     printf("Hello, World!\n");
     return 0;
 }
